a8267a49531bbb8cc9e1bd36903d380ad133cca7
Sarah Xie <83103191+sxiee@users.noreply.github.com>
2023-12-05 15:16:57 -0500

    Update app.js

--- kanbas-node-server-app/app.js
+++ kanbas-node-server-app/app.js
@@ -1,14 +1,38 @@
+// const express = require("express");
+import session from "express-session";
+import mongoose from "mongoose";
 import express from "express";
-import Hello from "./hello.js";
+import HelloRoutes from "./hello.js";
 import Lab5 from "./lab5.js";
-import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
+import UserRoutes from "./users/routes.js";
+import cors from "cors";
+
+
 const app = express();
+const DB_CONNECTION_STRING = "mongodb+srv://sarahxie:<password>@cluster0.1gsp0ii.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(session(sessionOptions));
+
 app.use(express.json());
-ModuleRoutes(app);
-app.use(cors());
+
+UserRoutes(app);
 CourseRoutes(app);
 Lab5(app);
-Hello(app);
+HelloRoutes(app);
+
 app.listen(4000);
